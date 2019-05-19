6a3c3ba7520e753d8cd7133cb0e960a32fc25953
Administrator <Administrator@CY-20160121YULK>
2019-05-19 11:15:15 +0800

    first add to git

---
+++ src/main/java/com/jd/SpringBoot001StarterApplication.java
@@ -0,0 +1,15 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot001StarterApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBoot001StarterApplication.class, args);
+		
+	}
+
+}
